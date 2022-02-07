e7fe4dba405d0a99aeb8566e51748defb69047c0
Danijel Kelava <danijel.kelava@goldinteractive.ch>
2022-02-07 18:02:42 +0100

    Update base relations query

--- src/services/SafeDeleteService.php
+++ src/services/SafeDeleteService.php
@@ -300,7 +300,6 @@ class SafeDeleteService extends Component
                             ->innerJoin('{{%content}} as content', 'content.elementId = relations.targetId')
                             ->innerJoin('{{%sites}} as sites', 'sites.id = content.siteId')
                             ->where(['=', 'relations.targetId', $id])
-                            ->groupBy('elements.id')
                             ->all();
     }
 
